df8b046d79398b12f873d431ae8a38dd645a87b8
peterbebawy <115377883+peterbebawy@users.noreply.github.com>
2026-09-13 18:49:20 +0300

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,20 @@
+/*
+  ضع هنا بيانات Web App التي تحصل عليها من:
+  Firebase Console > Project settings > Your apps > Web app
+
+  لا تغيّر اسم window.SPEECH_FIREBASE.
+*/
+window.SPEECH_FIREBASE = {
+  config: {
+    apiKey: "PUT_YOUR_API_KEY_HERE",
+    authDomain: "PUT_YOUR_PROJECT_ID.firebaseapp.com",
+    projectId: "PUT_YOUR_PROJECT_ID",
+    storageBucket: "PUT_YOUR_PROJECT_ID.firebasestorage.app",
+    messagingSenderId: "PUT_YOUR_MESSAGING_SENDER_ID",
+    appId: "PUT_YOUR_APP_ID"
+  },
+
+  // يمكنك تعديلهما كما تريد
+  clinicId: "main-clinic",
+  clinicName: "مركز التخاطب"
+};
