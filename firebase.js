71601f4aa781cd247b1bec29cba8b61b06e14689
peterbebawy <115377883+peterbebawy@users.noreply.github.com>
2026-09-13 18:51:54 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,21 @@
-/*
-  ضع هنا بيانات Web App التي تحصل عليها من:
-  Firebase Console > Project settings > Your apps > Web app
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-  لا تغيّر اسم window.SPEECH_FIREBASE.
-*/
-window.SPEECH_FIREBASE = {
-  config: {
-    apiKey: "PUT_YOUR_API_KEY_HERE",
-    authDomain: "PUT_YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "PUT_YOUR_PROJECT_ID",
-    storageBucket: "PUT_YOUR_PROJECT_ID.firebasestorage.app",
-    messagingSenderId: "PUT_YOUR_MESSAGING_SENDER_ID",
-    appId: "PUT_YOUR_APP_ID"
-  },
-
-  // يمكنك تعديلهما كما تريد
-  clinicId: "main-clinic",
-  clinicName: "مركز التخاطب"
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA-S8_HLnl8XdU37MS2nZjqNdQSU1vqTYM",
+  authDomain: "speech-therapy-e68ee.firebaseapp.com",
+  projectId: "speech-therapy-e68ee",
+  storageBucket: "speech-therapy-e68ee.firebasestorage.app",
+  messagingSenderId: "766272352101",
+  appId: "1:766272352101:web:56874039315d00866b3b8c",
+  measurementId: "G-VZF7TH20M9"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
